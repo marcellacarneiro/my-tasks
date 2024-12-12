9bc0c345b5e0ef9d6b2346c8422680c2f2550326
marcellacarneiro <carneiromarcella53@gmail.com>
2024-12-12 16:30:42 -0300

    feat: adds lucide icons, line-through decoration and toggle status

--- src/components/ItemList/index.js
+++ src/components/ItemList/index.js
@@ -1,15 +1,22 @@
+import { X, Check } from 'lucide-react';
 import styles from './ItemList.module.css';
 
-function ItemList({ title, status, description }) {
+function ItemList({ title, status, description, toggleStatus }) {
+    const taskIsDone = status === 'done';
+    const Icon = taskIsDone ? Check : X;
+
     return (
         <>
             <div className={styles.itemList}>
                 <div>
-                    <h2>{title}</h2>
-                    <p className={styles.description}>{description}</p>
+                    <h2 style={{ textDecoration: taskIsDone ? 'line-through' : 'none' }}>{title}</h2>
+                    <p className={styles.description} style={{ textDecoration: taskIsDone ? 'line-through' : 'none' }}>
+                        {description}
+                    </p>
                 </div>
                 <div>
-                    <div className={status === 'done' ? styles.done : styles.pending}>
+                    <div className={taskIsDone ? styles.done : styles.pending} onClick={toggleStatus}>
+                        <Icon style={{ width: '17px' }} />
                         <p>{status}</p>
                     </div>
                 </div>
