447ffc0650166991844d7d8614e2241a9fe25995
marcellacarneiro <carneiromarcella53@gmail.com>
2025-01-05 13:59:06 -0300

    fix: removes predefined tasks at mockDatabase.js

--- src/mockDatabase.js
+++ src/mockDatabase.js
@@ -1,42 +1,42 @@
 export const mockDatabase = {
     tasks: [
-        {
-            id: 1,
-            title: 'Complete weekly report',
-            description: 'Complete the weekly progress report and send it to the manager before the deadline.',
-            status: 'done',
-        },
-        {
-            id: 2,
-            title: 'Review website design',
-            description: 'Review the visual elements of the new website layout with the design team.',
-            status: 'done',
-        },
-        {
-            id: 3,
-            title: 'Plan team meeting',
-            description: 'Define the agenda and prepare slides for the Monday meeting.',
-            status: 'pending',
-        },
-        {
-            id: 4,
-            title: 'Buy groceries',
-            description: 'Buy bread, milk, and eggs at the supermarket.',
-            status: 'pending',
-        },
-        {
-            id: 5,
-            title: 'Plan annual conference',
-            description: `Organize the company's annual conference. The tasks include:
-- Book the event venue, ensuring enough space for all attendees.
-- Send invitations to keynote speakers and confirm their attendance.
-- Create a detailed agenda, including times for lectures, workshops, and breaks.
-- Hire a support team to handle technology, sound, and projection.
-- Coordinate catering for lunch, coffee breaks, and closing cocktail.
-- Develop promotional materials, such as banners, flyers, and custom giveaways.
-- Set up an online registration system to facilitate participant sign-up.
-- Ensure that all safety measures and protocols comply with local regulations.`,
-            status: 'pending',
-        },
+//         {
+//             id: 1,
+//             title: 'Complete weekly report',
+//             description: 'Complete the weekly progress report and send it to the manager before the deadline.',
+//             status: 'done',
+//         },
+//         {
+//             id: 2,
+//             title: 'Review website design',
+//             description: 'Review the visual elements of the new website layout with the design team.',
+//             status: 'done',
+//         },
+//         {
+//             id: 3,
+//             title: 'Plan team meeting',
+//             description: 'Define the agenda and prepare slides for the Monday meeting.',
+//             status: 'pending',
+//         },
+//         {
+//             id: 4,
+//             title: 'Buy groceries',
+//             description: 'Buy bread, milk, and eggs at the supermarket.',
+//             status: 'pending',
+//         },
+//         {
+//             id: 5,
+//             title: 'Plan annual conference',
+//             description: `Organize the company's annual conference. The tasks include:
+// - Book the event venue, ensuring enough space for all attendees.
+// - Send invitations to keynote speakers and confirm their attendance.
+// - Create a detailed agenda, including times for lectures, workshops, and breaks.
+// - Hire a support team to handle technology, sound, and projection.
+// - Coordinate catering for lunch, coffee breaks, and closing cocktail.
+// - Develop promotional materials, such as banners, flyers, and custom giveaways.
+// - Set up an online registration system to facilitate participant sign-up.
+// - Ensure that all safety measures and protocols comply with local regulations.`,
+//             status: 'pending',
+//         },
     ]
 }
